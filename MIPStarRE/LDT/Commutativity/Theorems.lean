cb55d4aa8d49b22876cee942e7c153c7aa4e281f
Ray <divergence_lu@icloud.com>
2026-05-25 04:35:09 +0200

    refactor(LDT/Commutativity): trim theorem root imports (#2002)

--- MIPStarRE/LDT/Commutativity/Theorems.lean
+++ MIPStarRE/LDT/Commutativity/Theorems.lean
@@ -1,9 +1,3 @@
-import MIPStarRE.LDT.Commutativity.Scaffold
-import MIPStarRE.LDT.Commutativity.EvaluatedSliceBounds
-import MIPStarRE.LDT.Commutativity.EvaluatedSliceCommutation
-import MIPStarRE.LDT.Commutativity.ScalarApproximation
-import MIPStarRE.LDT.Commutativity.GCommStability
-import MIPStarRE.LDT.Commutativity.Transport
 import MIPStarRE.LDT.Commutativity.Main
 
 /-!
