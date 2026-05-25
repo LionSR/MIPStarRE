59c0ab8cedadb402df0d560f1532dc9d2fc76544
Sirui@MPQ <divergence_lu@icloud.com>
2026-05-25 16:58:17 +0200

    refactor(LDT/Pasting): remove commuting-with-G barrel

--- MIPStarRE/LDT.lean
+++ MIPStarRE/LDT.lean
@@ -62,7 +62,7 @@ import MIPStarRE.LDT.Pasting.Core
 import MIPStarRE.LDT.Pasting.SwitcherooSetup
 import MIPStarRE.LDT.Pasting.SwitcherooContraction
 import MIPStarRE.LDT.Pasting.SwitcherooCompletion
-import MIPStarRE.LDT.Pasting.CommutingWithG
+import MIPStarRE.LDT.Pasting.CommutingWithG.Incomplete
 import MIPStarRE.LDT.Pasting.GHatFacts
 import MIPStarRE.LDT.Pasting.BridgeLemmas
 import MIPStarRE.LDT.Pasting.Bernoulli
