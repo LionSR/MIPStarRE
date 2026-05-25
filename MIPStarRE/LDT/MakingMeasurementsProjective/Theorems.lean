a8681ea4e0ec807afe112af6c50c3591916fc4f9
Sirui@MPQ <divergence_lu@icloud.com>
2026-05-25 05:15:21 +0200

    refactor(LDT/Projective): trim theorem aggregate imports

--- MIPStarRE/LDT/MakingMeasurementsProjective/Theorems.lean
+++ MIPStarRE/LDT/MakingMeasurementsProjective/Theorems.lean
@@ -1,5 +1,3 @@
-import MIPStarRE.LDT.MakingMeasurementsProjective.NaimarkCore
-import MIPStarRE.LDT.MakingMeasurementsProjective.NaimarkOneMeas
 import MIPStarRE.LDT.MakingMeasurementsProjective.NaimarkFull
 import MIPStarRE.LDT.MakingMeasurementsProjective.Orthonormalization
 
